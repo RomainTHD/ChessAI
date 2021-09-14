a912559c2be04ac8c7fb5993c4b442324b919e4d
Romain <romain.theodet@hotmail.fr>
2021-09-14 17:32:46 +0200

    new: Pawn movements (WIP)

--- src/model/pieces/Pawn.ts
+++ src/model/pieces/Pawn.ts
@@ -1,7 +1,10 @@
-import {Piece} from "model";
-import {Color} from "model/Color";
-import {Move} from "model/Move";
-import {Type} from "model/Type";
+import {
+    Color,
+    Move,
+    Piece,
+    Position,
+    Type,
+} from "model";
 
 class Pawn extends Piece {
     public readonly type = Type.Pawn;
@@ -11,14 +14,15 @@ class Pawn extends Piece {
         const direction = this.color === Color.White ? 1 : -1;
         const moves     = [] as Move[];
 
-        for (let i = 1; i <= 2; ++i) {
-            if (this.position.row + i >= this.board.NB_ROWS || this.position.row + i < 0) {
-                break;
-            }
+        let upperBound = 1;
+        if ((this.color === Color.White && this.position.row === 1) ||
+            (this.color === Color.Black && this.position.row === this.board.NB_ROWS - 2)
+        ) {
+            upperBound = 2;
+        }
 
-            if (this.board.getPiece(this.position.row + i * direction, this.position.col) === null) {
-                moves.push(Move.fromOffset(this, i * direction, 0));
-            }
+        for (let i = 1; i <= upperBound; ++i) {
+            this._addMoveIfAvailable(Position.add(this.position, new Position(i * direction, 0)), moves);
         }
 
         return moves;
