696fb02bbd595f8ee00d2843ab7e2a0bec6ed617
Romain <romain.theodet@hotmail.fr>
2021-09-14 14:08:30 +0200

    change: Color as a "real" enum

--- src/model/Color.ts
+++ src/model/Color.ts
@@ -1,20 +1,6 @@
-class Color {
-    public static readonly Black = new Color("black");
-    public static readonly White = new Color("white");
-
-    private readonly _color: string;
-
-    private constructor(color: string) {
-        this._color = color;
-    }
-
-    public get color(): string {
-        return this._color;
-    }
-
-    public toString(): string {
-        return this.color;
-    }
+enum Color {
+    Black = "black",
+    White = "white",
 }
 
 export {Color};
