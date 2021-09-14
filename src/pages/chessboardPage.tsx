71400db485ae4cfdbde303082c8a0df07886ad3c
Romain <romain.theodet@hotmail.fr>
2021-09-14 23:00:52 +0200

    test: Initial FEN

--- src/pages/chessboardPage.tsx
+++ src/pages/chessboardPage.tsx
@@ -10,7 +10,7 @@ class ChessboardPage extends React.Component<{}, {}> {
                 <div className="row justify-content-center">
                     <div className="col-auto">
                         <ChessboardComponent
-                            chessboard={new Chessboard()}
+                            chessboard={new Chessboard("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0")}
                         />
                     </div>
                 </div>
