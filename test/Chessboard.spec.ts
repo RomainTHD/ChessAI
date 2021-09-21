e6a7d9924aabf181a9fbada262d433e26e5842b5
Romain <romain.theodet@hotmail.fr>
2021-09-21 03:10:36 +0200

    new: Tests for the right amount of possible moves

--- test/Chessboard.spec.ts
+++ test/Chessboard.spec.ts
@@ -5,10 +5,47 @@ import {
     Color,
 } from "model";
 
-describe("Initial color", () => {
+describe("Available moves for start position", () => {
+    const FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
     it("should be White turn", () => {
-        const board = new Chessboard();
-        const color = board.activeColor;
-        expect(color).to.equal(Color.White);
+        const board = new Chessboard(FEN);
+        expect(board.activeColor).to.equal(Color.White);
+    });
+
+    it("should have the right number of pieces", () => {
+        const board = new Chessboard(FEN);
+        expect(board.getPieces(Color.Black).length).to.equal(16);
+        expect(board.getPieces(Color.White).length).to.equal(16);
+        expect(board.getAllPieces().length).to.equal(32);
+    });
+
+    it("should detect the right amount of moves - depth 0", () => {
+        const board    = new Chessboard(FEN);
+        let movesCount = 0;
+        for (const piece of board.getPieces(board.activeColor)) {
+            movesCount += piece.getLegalMoves().length;
+        }
+
+        expect(movesCount).to.equal(20);
+    });
+
+    it("should detect the right amount of moves - depth 1", () => {
+        // FIXME: Messy
+        const board    = new Chessboard(FEN);
+        let movesCount = 0;
+        for (const piece of board.getPieces(board.activeColor)) {
+            const moves = piece.getLegalMoves();
+            for (const move of moves) {
+                board.tryMove(move, (subBoard) => {
+                    for (const subPiece of subBoard.getPieces(subBoard.activeColor)) {
+                        const subMoves = subPiece.getLegalMoves();
+                        movesCount += subMoves.length;
+                    }
+                });
+            }
+        }
+
+        expect(movesCount).to.equal(400);
     });
 });
